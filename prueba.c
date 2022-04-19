9d6c14aec4d52e10c78cfc4d3e9563e6bdcdc9da
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2022-04-19 16:24:02 -0300

    Test ciclos

--- prueba.c
+++ prueba.c
@@ -8,10 +8,83 @@ int main(){
 
 	init(v);
 
-	for (int i = 0; i < capacity(v); ++i)
+	printf("%d\n", capacity(v));
+
+	for (int i = 0; i < *v->_capacity; ++i)
 	{
 		printf("hola mundo\n");
 	}
 
+	free(v->p);
 	return 0;
 }
+
+/*
+//PARA PRUEBAS SIN CICLOS
+
+	printf("%d\n", at(v,0));
+	printf("%d\n", size(v));
+	printf("%d\n", capacity(v));
+
+	push(v,178);
+	push(v,178);
+	push(v,178);
+	push(v,65);
+
+	printf("\n%d\n", at(v,0));
+	printf("%d\n", size(v));
+	printf("%d\n", capacity(v));
+
+	insert(v,3,135);
+	prepend(v,67);
+	push(v,156);
+	push(v,182);
+	push(v,193);
+	push(v,15);
+	push(v,16);
+	push(v,56);
+	push(v,556);
+	push(v,1);
+	push(v,6);
+	push(v,5);
+
+	printf("\n%d\n", find(v,135));
+	printf("%d\n", find(v,178));
+
+	printf("\n%d\n", at(v,0));
+	printf("%d\n", at(v,1));
+	printf("%d\n", at(v,2));
+	printf("%d\n", at(v,3));
+	printf("%d\n", at(v,4));
+	printf("%d\n", at(v,5));
+	printf("%d\n", at(v,6));
+	printf("%d\n", at(v,7));
+	printf("%d\n", at(v,8));
+	printf("%d\n", at(v,9));
+	printf("%d\n", at(v,10));
+	printf("%d\n", at(v,11));
+	printf("%d\n", at(v,12));
+	printf("%d\n", at(v,13));
+	printf("%d\n", at(v,14));
+	printf("%d\n", at(v,15));
+
+	printf("\n%d\n", pop(v));
+
+	printf("\n%d\n", size(v));
+	printf("%d\n", capacity(v));
+	printf("%d\n", isEmpty(v));
+
+	printf("\n%d\n", pop(v));
+	printf("%d\n", pop(v));
+	printf("%d\n", pop(v));
+	printf("%d\n", pop(v));
+	printf("%d\n", pop(v));
+	printf("%d\n", pop(v));
+	printf("%d\n", pop(v));
+	printf("%d\n", pop(v));
+	printf("%d\n", pop(v));
+	printf("%d\n", pop(v));
+
+	printf("\n%d\n", size(v));
+	printf("%d\n", capacity(v));
+*/
