b563277f22ce5c4d79e51bc1976292abb294d650
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2022-04-21 15:39:35 -0300

    Simplificación en pushBack().

--- ListaSimple.h
+++ ListaSimple.h
@@ -130,25 +130,24 @@ void pushBack(struct SLinkedList *l, int value){
 	if (size(l) == 0) {
 		l->head = n;
 	}
-	else if (size(l) == 1){
-		l->head->next = n;
-	}
-	else if (size(l) >= 2){
+	else {
 		for (int i = 0; i < size(l)-1; i++) {
 			l->head = l->head->next;
 		}
-
 		l->head->next = n;
-		l->head = aux;
 	}
 
+	l->head = aux;
 	n->next = NULL;
 	n->_value = value;
 	incrementSize(l);
 }
 
 int popBack(struct SLinkedList *l){
+	int aux;
 
+	decrementSize(l);
+	return aux;
 }
 
 ///////////////////////////////////////////////////////////FIN///////////////////////////////////////////////////////////
